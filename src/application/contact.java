4eb9ad2c0e8266e521e9a2ded25f1a96be986a56
JX0Z <32142980+JX0Z@users.noreply.github.com>
2019-10-30 19:11:11 -0400

    Fixed naming convention

--- src/application/contact.java
+++ src/application/contact.java
@@ -5,32 +5,32 @@ public class contact {
 	private String lastName;
 	
 	private String email;
-	public contact(String n1, String n2) {
-		this.firstName=n1;
-		this.lastName=n2;
+	public contact(String firstName, String lastName) {
+		this.firstName=firstName;
+		this.lastName=lastName;
 	}
-	public contact(String n1, String n2, String e) {
-		this.firstName=n1;
-		this.lastName=n2;
-		this.email=e;
+	public contact(String firstName, String lastName, String email) {
+		this.firstName=firstName;
+		this.lastName=lastName;
+		this.email=email;
 	}
 	
 	public String getFirstName() {
 		return(this.firstName);
 	}
-    public void setFirstName(String n) {
-        this.firstName = n;
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
     }
 	public String getlastName() {
 		return(this.lastName);
 	}
-    public void setLastName(String n) {
-        this.lastName = n;
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
     }
 	public String getEmail() {
 		return(this.email);
 	}
-	public void setEmail(String e) {
-		this.email = e;
+	public void setEmail(String email) {
+		this.email = email;
 	}
 }
