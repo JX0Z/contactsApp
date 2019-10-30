46cd2ffae7d4a353d3be1ae451a42380eec2ad68
JX0Z <32142980+JX0Z@users.noreply.github.com>
2019-10-30 19:22:21 -0400

    Added middleName & Company

--- src/application/contact.java
+++ src/application/contact.java
@@ -2,9 +2,13 @@ package application;
 
 public class contact {
 	private simpleStringProperty firstName;
+	private SimpleStringProperty middleName;
 	private SimpleStringProperty lastName;
 	
+	private SimpleStringProperty company;
+	
 	private SimpleStringProperty email;
+	
 	public contact(SimpleStringProperty firstName, SimpleStringProperty lastName) {
 		this.firstName=firstName;
 		this.lastName=lastName;
@@ -18,19 +22,31 @@ public class contact {
 	public SimpleStringProperty getFirstName() {
 		return(this.firstName);
 	}
-    public void setFirstName(SimpleStringProperty firstName) {
-        this.firstName = firstName;
-    }
+    	public void setFirstName(SimpleStringProperty firstName) {
+        	this.firstName = firstName;
+   	}
 	public SimpleStringProperty getlastName() {
 		return(this.lastName);
 	}
-    public void setLastName(SimpleStringProperty lastName) {
-        this.lastName = lastName;
-    }
+ 	public void setLastName(SimpleStringProperty lastName) {
+        	this.lastName = lastName;
+	}
+	public SimpleStringProperty getMiddleName() {
+		return(this.middleName);
+	}
+ 	public void setMiddleName(SimpleStringProperty middleName) {
+        	this.middleName = middleName;
+	}
 	public SimpleStringProperty getEmail() {
 		return(this.email);
 	}
 	public void setEmail(SimpleStringProperty email) {
 		this.email = email;
+	}
+	public SimpleStringProperty getCompany() {
+		return(this.company);
+	}
+	public void setCompany(SimpleStringProperty company) {
+		this.company = company;
 	}
 }
