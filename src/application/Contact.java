3465de8a0ef0efc2736e32706eccd386d32b07e9
JX0Z <32142980+JX0Z@users.noreply.github.com>
2019-10-31 12:21:58 -0400

    Added csv loading functionality
    - Added Contact.loadCSV method
    - Will read values from data.txt and return them in an observable list
    To Do:
    - Figure out how to save objects to CSV

--- src/application/Contact.java
+++ src/application/Contact.java
@@ -1,7 +1,17 @@
+
 package application;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
 public class Contact {
-	private simpleStringProperty firstName;
+	private SimpleStringProperty firstName;
 	private SimpleStringProperty middleName;
 	private SimpleStringProperty lastName;
 	
@@ -9,17 +19,17 @@ public class Contact {
 	
 	private SimpleStringProperty email;
 	
-	public contact(String firstName, String lastName) {
+	public Contact(String firstName, String lastName) {
 		this.firstName = new SimpleStringProperty(firstName);
 		this.lastName = new SimpleStringProperty(lastName);
 	}
-	public contact(String firstName, String lastName, String email) {
+	public Contact(String firstName, String lastName, String email) {
 		this.firstName = new SimpleStringProperty(firstName);
 		this.lastName = new SimpleStringProperty(lastName);
 		this.email = new SimpleStringProperty(email);
 	}
 	
-	public contact(String firstName, String lastName, String email) {
+	public Contact(String firstName, String middleName, String lastName, String company, String email) {
 		this.firstName = new SimpleStringProperty(firstName);
 		this.middleName = new SimpleStringProperty(middleName);
 		this.lastName = new SimpleStringProperty(lastName);
@@ -27,23 +37,41 @@ public class Contact {
 		this.email = new SimpleStringProperty(email);
 	}
 	
+	private static File file = new File ("src/application/data.txt");
+	
+	public static ObservableList<Contact> loadCSV() throws Exception {
+		ObservableList<Contact> list = FXCollections.observableArrayList();
+		BufferedReader br = new BufferedReader(new FileReader(Contact.file));
+		String line = br.readLine();
+		while (line != null) {
+			String[] fields = line.split(",");
+			Contact contact = new Contact(fields[0],fields[1],fields[2],fields[3],fields[4]);
+			list.add(contact);
+			line = br.readLine();
+		}
+		return list;
+	}
+	
+	public static void saveCSV(ObservableList<Contact> list) {
+		//To Do: Complete this method to write list to Contact.file
+	}
 	public SimpleStringProperty getFirstName() {
 		return(this.firstName);
 	}
-    	public void setFirstName(String firstName) {
-        	this.firstName.set(firstName);
-   	}
-	public SimpleStringProperty getlastName() {
-		return(this.lastName);
+	public void setFirstName(String firstName) {
+    	this.firstName.set(firstName);
 	}
- 	public void setLastName(String lastName) {
-        	this.lastName.set(lastName);
+	public SimpleStringProperty getlastName() {
+	return(this.lastName);
+	}
+	public void setLastName(String lastName) {
+    	this.lastName.set(lastName);
 	}
 	public SimpleStringProperty getMiddleName() {
 		return(this.middleName);
 	}
- 	public void setMiddleName(String middleName) {
-        	this.middleName.set(middleName);
+	public void setMiddleName(String middleName) {
+    	this.middleName.set(middleName);
 	}
 	public SimpleStringProperty getEmail() {
 		return(this.email);
@@ -56,5 +84,5 @@ public class Contact {
 	}
 	public void setCompany(String company) {
 		this.company.set(company);
-	}
+}
 }
